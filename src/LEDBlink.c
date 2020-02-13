3eef168eebeaa2f9c7ce0472f857ab26e3a5e203
RKouhei <kohriki222@gmail.com>
2020-02-13 10:56:44 +0900

    Imitation of pedestrian signals

--- src/LEDBlink.c
+++ src/LEDBlink.c
@@ -22,14 +22,23 @@ int main(void) {
 
 	// PIO0_2: output
 	LPC_GPIO_PORT->DIR0 |= (1<<2);
+	LPC_GPIO_PORT->DIR0 |= (1<<4);
 
 	// Force the counter to be placed into memory
 	volatile static int i = 0 ;
+	volatile static int x = 0 ;
 	// Enter an infinite loop, just incrementing a counter
 	while(1) {
-		for (i=0; i<100000; i++);
-		// Toggle PIO0_2
 		LPC_GPIO_PORT->NOT0 = 1<<2;
+		for (x = 0; x < 1000000; x++);
+		LPC_GPIO_PORT->NOT0 = 1<<2;
+		for (x = 0; x < 10; x++) {
+			LPC_GPIO_PORT->NOT0 = 1<<2;
+			for (i=0; i<100000; i++);
+		}
+		LPC_GPIO_PORT->NOT0 = 1<<4;
+		for (x = 0; x < 1000000; x++);
+		LPC_GPIO_PORT->NOT0 = 1<<4;
 	}
 	return 0 ;
 }
